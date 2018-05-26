90d661b389986cbff94fb13e38cc557e0741a318
bilegou <2478969052@qq.com>
2018-05-26 10:23:43 +0800

    beta5

--- laravel/resources/views/static_pages/about.blade.php
+++ laravel/resources/views/static_pages/about.blade.php
@@ -1,8 +1,5 @@
-<html>
-<head>
-    <title>Sample App</title>
-</head>
-<body>
-<h1>关于页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title','关于')
+@section('content')
+    <h1>关于页</h1>
+@stop
